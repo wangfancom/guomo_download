bf85ed23a6021035e2172d8fb462bebd3aa41e91
wangfan_tju <wangfan_tju@163.com>
2018-10-28 15:46:24 +0800

    change main_path

--- downloadgm.py
+++ downloadgm.py
@@ -5,7 +5,7 @@ import os
 page_num = 98 * 2
 for i in range(1, page_num + 1):
   s = str(i) 
-  main_path = 'http://p.9grenti.org/uploadfile/2018/0926/07/'
+  main_path = 'http://needtodown.org/uploadfile/2018/0926/07/'
   if i < 10:
     s = s.zfill(2)
   if not os.path.exists(s.zfill(2) + '.jpg'):
